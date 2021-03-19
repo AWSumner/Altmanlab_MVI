c7606b2779d025377c208d3c8a6b7ed9e5ebc9bd
AWSumner <68126412+AWSumner@users.noreply.github.com>
2021-03-19 12:48:40 -0700

    Add files via upload

---
+++ A_S_model/Reference/Generators/FCN_short_Generator.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 18 09:14:28 2021
+
+@author: sumphys
+"""
+'Imports'
+import numpy as np  
+import os.path #used in creation of final path
+import random 
+from sympy import *
+import time
+
+z=symbols('z')#symbol used in the functions
+
+start_time = time.time() #timers are fun!
+
+'inputs'
+x_dim=300 #number of entries per row
+y_dim=300 #number of rows
+Func=[5*sin(z)+40,5*cos(z)+z+3] # the function(s) you are mapping. Note: if you are using a non cont. and/or non diff.able function this code can produce errors (also what is wrong with you?! )
+Map_name='Fn_test_3' #name of map file you want
+Directionality=True #check that determines if you want a 'backwards direction' to be possible ((theta can be (pi/2->pi->-pi/2))) from arctan codomain
+
+'Save paths'
+end_path='./Reference/Maps/' #Final location of the map just in case you want to change it
+'Note: you are running this is A_S model the . represents the path the the running directory '
+Final_location=os.path.join(end_path,Map_name+'.csv') #creates path for saving the final file
+
+'Map'
+Matrix=np.zeros((y_dim,x_dim))
+
+'Creating the map'
+
+R=0 #reset the R counter **left over for the reverse list method at the bottom
+for Fcn in Func: #does the mapping for each function
+    deriv=diff(Fcn,z,1) #Takes derivative with respect to z once
+    if Directionality: #checks if you want directionality for a given function
+        reverse=random.choice([True,False]) #if you want it to randomly choose for each function
+       #reverse=True  #if you want to fix it True
+       #reverse=False #if you want to fix it False
+       
+    y=0 # y position index reset
+    for row in Matrix: # seperates out each row
+    
+        x=0 # x position indexing reset
+        for item in row: # looks at each item in that row
+        
+            if (y-1<=Fcn.subs(z,x)<=y+1): #or (Fcn.subs(z,(x-1))<=y<=Fcn.subs(z,(x+1)) or (Fcn.subs(z,(x+1))<=y<=Fcn.subs(z,(x-1)))): # check to see if the function (+-1) bound the y value or the y(+-1) values bound the function values 
+                slp=deriv.subs(z,x) # evaluates the slope of the function by subing in the x value
+                theta=np.arctan(float(slp))
+                 
+                if theta <=0: #converts the angle to match the reqirements for the model
+                    theta+=2*np.pi #adds two pi
+                
+                if reverse: 
+                    theta+=(-np.pi) # takes the calculated theta and converts it to its reverse
+                
+                if item==0: #checks to see if the entry is empty
+                    Matrix[y,x]=theta #if empty it fills the position with
+                
+                if item !=0: #check to see if it is not Note: Not an else because it could lead to an error with two ifs above it. 
+                    rand=[theta,item] #makes a list of potential values
+                    Matrix[y,x]=random.choice(rand) #randomly chooses which value to use at that intersection
+                    
+                    
+            x+=1    #increase index by one
+        y+=1 #increase y index by one
+        print(R+1,len(Func),y) #lets you see progress while it runs
+    R+=1 #increase R  by one **left over for the reverse list method at the bottom
+    
+
+#print(Matrix) #testing
+
+
+np.savetxt(Final_location,Matrix)
+
+print('Done')
+print("--- %s seconds ---" % (time.time() - start_time)) #what time is it? 
+
+'''
+reverse=[] #reverse list reset
+if Directionality: #checks if you want directionailty
+    for i in range(len(Func)):  #makes a value for each function
+        rev=random.choice([True,False]) # chooses the function
+        reverse.append(rev) #adds the rev value to the reverse list
+
+#reverse=[] #Here for if you want to set the reverse list manually
+'''
